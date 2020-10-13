25130d8791ae8f465e073180ca37f8c78a1075a2
Jim <jsmith774@gmail.com>
2020-10-13 16:58:05 -0400

    initial commit of user endpoint

--- src/app.js
+++ src/app.js
@@ -6,21 +6,23 @@ const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const errorHandler = require('./middleware/error-handler');
 const authRouter = require('./auth/auth-router');
- 
+const userRouter = require('./user/user-router');
 
 const app = express();
 
-app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
-  skip: () => NODE_ENV === 'test',
-}));
-app.get('/', (req, res, next) => {
-  res.send('Hello, burnt_toast');
+app.use(
+  morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test',
+  })
+);
+app.get('/', (req, res) => {
+  res.send('Hello, burnt_toast'); //TODO: make this self-documenting API
 });
+
 app.use(cors());
 app.use(helmet());
 app.use('/api/auth', authRouter);
- 
- 
+app.use('/api/user', userRouter);
 
 app.use(errorHandler);
 
