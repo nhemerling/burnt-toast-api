9936cbabc78f3dc098c4e8d7acf4a7f33e9f370c
Jim <jsmith774@gmail.com>
2020-10-15 17:20:51 -0400

    hotfix merge error

--- src/userSkills/user-skills-router.js
+++ src/userSkills/user-skills-router.js
@@ -12,15 +12,17 @@ userSkillsRouter
   .route('/')
   .get(async (req, res, next) => {
     try {
-      const allUsersAndSkills = await UserSkillsService.getAllUsersAndSkills(req.app.get('db'));
+      const allUsersAndSkills = await UserSkillsService.getAllUsersAndSkills(
+        req.app.get('db')
+      );
       res.status(200).json(allUsersAndSkills);
     } catch (error) {
-      next(error)
+      next(error);
     }
   })
   .post(jsonBodyParser, async (req, res, next) => {
     try {
-      const {skill_id, skill_desc, skill_img_url} = req.body;
+      const { skill_id, skill_desc, skill_img_url } = req.body;
       const id = req.user.id;
       if (!skill_id) {
         return res.status(400).json({
@@ -31,12 +33,15 @@ userSkillsRouter
         fk_user_id: id,
         fk_skill_id: skill_id,
         primary_img_url: skill_img_url,
-        primary_description: skill_desc
-      }
-      const userSkill = await UserSkillsService.postLinkUserSkills(req.app.get('db'), linkUserSkill);
+        primary_description: skill_desc,
+      };
+      const userSkill = await UserSkillsService.postLinkUserSkills(
+        req.app.get('db'),
+        linkUserSkill
+      );
       res.status(201).json(userSkill);
     } catch (error) {
-      next(error)
+      next(error);
     }
     const linkUserSkill = {
       fk_user_id: id,
@@ -49,10 +54,7 @@ userSkillsRouter
       linkUserSkill
     );
     res.status(201).json(userSkill);
-  } catch (error) {
-    next(error);
-  }
-});
+  });
 userSkillsRouter.route('/:user_id').get(async (req, res, next) => {
   try {
     const id = req.user.id;
